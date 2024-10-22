2ba08794d3323b0f006c6567d6b6a189324453f5
cosmos <alfredchrisayo@gmail.com>
2024-10-22 17:18:15 +0000

    save changes

--- src/Components/Pages/Landing/Components/SupportSection.tsx
+++ src/Components/Pages/Landing/Components/SupportSection.tsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import image from "../../../../assets/Mother.webp";
+// import image from "../../../../assets/Mother.webp";
 import { Pagination } from "swiper/modules";
 import { ArrowRight } from "react-huge-icons/solid";
 import cardImage from "../../../../assets/wheelchair.webp";
