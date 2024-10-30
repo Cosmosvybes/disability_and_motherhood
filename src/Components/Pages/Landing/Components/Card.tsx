15aa49a49f1b74ef99f926d12fdcb01bc3d9dbd1
cosmos <alfredchrisayo@gmail.com>
2024-10-30 16:10:31 +0000

    save padding changes

--- src/Components/Pages/Landing/Components/Card.tsx
+++ src/Components/Pages/Landing/Components/Card.tsx
@@ -11,7 +11,7 @@ const Card_ = ({
     <>
       <div className="flex justify-between max-md:w-full h-96 max-md:h-auto max-sm:h-auto max-sm:flex-col  max-md:flex-col ">
         <div
-          className="relative max-md:w-full  max-md:h-96 max-sm:h-96 flex items-center max-md:object-contain justify-center  h-96 max-md:py-2  w-1/2"
+          className="relative max-md:w-full  max-md:h-96  max-sm:h-60 flex items-center max-md:object-contain justify-center  h-96 max-md:py-0 w-1/2"
           style={{
             backgroundImage: `url(${image})`,
             backgroundRepeat: "no-repeat",
@@ -23,16 +23,16 @@ const Card_ = ({
           <h1 className="text-3xl max-sm:text-xl max-md:text-xl font-extrabold">
             {researchTitle}
           </h1>
-          <div className="relative w-4/5   max-sm:w-full bg-gray-white  text-left rounded-xl px-0 max-sm:px-1 py-2 h-auto">
-            <p className="text-black max-sm:text-sm tex-xl font-bold">
+          <div className="relative w-4/5   max-sm:w-full bg-gray-white mt-2 text-left rounded-xl px-0 max-sm:px-0 py-2 max-md:py-0 h-auto">
+            <p className="text-black max-sm:text-sm text-xl font-bold">
               {content}
             </p>{" "}
           </div>
           <a
-            className="text-blue-500  max-sm:text-sm text-sm font-bold"
+            className="text-blue-500 max-sm:px-0  max-sm:text-sm text-sm font-bold"
             href="https://journals.sagepub.com/doi/epub/10.1177/26334941241288587"
           >
-            Read more here
+            read more here
           </a>
         </div>
       </div>
